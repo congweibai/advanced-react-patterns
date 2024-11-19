963cd6ae83e0d43f201c4c2b48c6d00427bf6b7e
Wyatt Bai <wyatt.bai@caremappr.com.au>
2024-11-19 16:09:54 +1030

    feat: control props

--- exercises/08.control-props/01.problem.control/toggle.tsx
+++ exercises/08.control-props/01.problem.control/toggle.tsx
@@ -27,21 +27,25 @@ export function useToggle({
 	// 🐨 add the `on` and `onChange` options here.
 	// 💰 you can alias it to `controlledOn` as we do in the Toggle component
 	// below to avoid "variable shadowing."
+	on: controlledOn,
+	onChange,
 	initialOn = false,
 	reducer = toggleReducer,
 }: {
 	// 🦺 add types for onChange and on here
 	initialOn?: boolean
 	reducer?: typeof toggleReducer
+	on?: boolean
+	onChange?: (state: ToggleState, action: ToggleAction) => void
 } = {}) {
 	const { current: initialState } = useRef<ToggleState>({ on: initialOn })
 	const [state, dispatch] = useReducer(reducer, initialState)
 	// 🐨 determine whether on is controlled and assign that to `onIsControlled`
 	// 💰 `controlledOn != null` // <-- note, using "!=" here instead of "!==" to count both `null` and `undefined` as uncontrolled.
-
+	const onIsControlled = controlledOn != null
 	// 🐨 Replace the next line with `const on = ...` which should be `controlledOn` if
 	// `onIsControlled`, otherwise, it should be `state.on`.
-	const { on } = state
+	const on = onIsControlled ? controlledOn : state.on
 
 	// We want to call `onChange` any time we need to make a state change, but we
 	// only want to call `dispatch` if `!onIsControlled` (otherwise we could get
@@ -51,6 +55,13 @@ export function useToggle({
 	// 1. accept an action
 	// 2. if onIsControlled is false, call dispatch with that action
 	// 3. Then call `onChange` with our "suggested changes" and the action.
+
+	function dispatchWithOnChange(action: ToggleAction) {
+		if (!onIsControlled) {
+			dispatch(action)
+		}
+		onChange?.(reducer({ ...state, on }, action), action)
+	}
 
 	// 🦉 "Suggested changes" refers to: the changes we would make if we were
 	// managing the state ourselves. This is similar to how a controlled <input />
@@ -68,8 +79,9 @@ export function useToggle({
 	// so keep that in mind when you call it! How could you avoid calling it if it's not passed?
 
 	// make these call `dispatchWithOnChange` instead
-	const toggle = () => dispatch({ type: 'toggle' })
-	const reset = () => dispatch({ type: 'reset', initialState })
+
+	const toggle = () => dispatchWithOnChange({ type: 'toggle' })
+	const reset = () => dispatchWithOnChange({ type: 'reset', initialState })
 
 	function getTogglerProps<Props>({
 		onClick,
@@ -115,6 +127,8 @@ export function Toggle({
 		// 🐨 forward on and onChange
 		// on: controlledOn,
 		// onChange
+		on: controlledOn,
+		onChange,
 	})
 	const props = getTogglerProps({ on })
 	return <Switch {...props} />
